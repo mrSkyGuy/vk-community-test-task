3c9ee87aa0f8fd2b394307aac85329743d123252
SkyGuy <nurmagomedoffzaur@yandex.ru>
2024-03-09 17:33:53 +0300

    init with linters and mock data

---
+++ src/shared/types/index.ts
@@ -0,0 +1,18 @@
+export interface IGetGroupsResponse {
+  result: 1 | 0;
+  data?: IGroup[];
+}
+
+export interface IGroup {
+  id: number;
+  name: string;
+  closed: boolean;
+  avatar_color?: string;
+  members_count: number;
+  friends?: IUser[];
+}
+
+export interface IUser {
+  first_name: string;
+  last_name: string;
+}
