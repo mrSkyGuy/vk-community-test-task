0c0c1751e4ef42835dbfb99898024c5c69e75ff9
SkyGuy <nurmagomedoffzaur@yandex.ru>
2024-03-12 21:47:22 +0300

    add beautiful loading and error :)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@
 // даже с помощью костылей починить
 
 import { useQuery } from "react-query";
-import { SplitCol, View, SplitLayout } from "@vkontakte/vkui";
+import { SplitCol, View, SplitLayout, Spinner, Button, Title } from "@vkontakte/vkui";
 import { apiClient } from "../../shared/api";
 import { GroupList } from "../../widgets/GroupList";
 import { useContext } from "react";
@@ -14,6 +14,7 @@ import { IsAlertOpenedContext } from "../../shared/context/IsAlertOpenedContext"
 import { Filters } from "../../features/Filters";
 import { Modal } from "./Modal";
 import { TGetGroupsResponse } from "../../shared/types";
+import { Icon56CancelCircleOutline } from "@vkontakte/icons";
 
 export function Home() {
   const { activePanel } = useContext(ActivePanelContext)!;
@@ -28,45 +29,52 @@ export function Home() {
     return colorsWD;
   }
 
-  const { status, data, error, refetch } = useQuery("group", () => apiClient.get("groups"), {
+  const { status, data, refetch, error } = useQuery("group", () => apiClient.get("groups"), {
     retry: 0
   });
 
-  switch (status) {
-    case "error":
-      return (
-        <span onClick={refetch}>
-          Error: {error.code}; {error.message}
-        </span>
-      );
-    case "loading":
-      return <span>loading</span>;
-    case "idle":
-      return <span>idle</span>;
+  return (
+    <SplitLayout
+      popout={alert}
+      modal={
+        status === "success" && (
+          <Modal>
+            <Filters id="filters" availableColors={getColorsWithoutDuplicates(data)} />
+          </Modal>
+        )
+      }
+    >
+      {status === "loading" && <Spinner size="large" style={{ margin: "auto" }} />}
 
-    case "success":
-      return (
-        <SplitLayout
-          popout={alert}
-          modal={
-            <Modal>
-              <Filters id="filters" availableColors={getColorsWithoutDuplicates(data)} />
-            </Modal>
-          }
+      {status === "error" && (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            margin: "auto"
+          }}
         >
-          <SplitCol maxWidth={500} style={{ margin: "auto" }}>
-            <View activePanel={activePanel}>
-              <GroupList id="group-list" groups={data} />
-              {data.map((group) => (
-                <GroupDescription
-                  id={`group--${group.id}`}
-                  key={`group--${group.id}`}
-                  info={group}
-                />
-              ))}
-            </View>
-          </SplitCol>
-        </SplitLayout>
-      );
-  }
+          <Icon56CancelCircleOutline />
+          <Title level="2">Код ошибки: {error.code}</Title>
+          <Title level="3">Сообщение ошибки: {error.message}</Title>
+          <Button size="l" onClick={refetch} style={{ marginTop: "8px" }}>
+            Попробовать еще раз
+          </Button>
+        </div>
+      )}
+
+      {status === "success" && (
+        <SplitCol maxWidth={500} style={{ margin: "auto" }}>
+          <View activePanel={activePanel}>
+            <GroupList id="group-list" groups={data} />
+            {data.map((group) => (
+              <GroupDescription id={`group--${group.id}`} key={`group--${group.id}`} info={group} />
+            ))}
+          </View>
+        </SplitCol>
+      )}
+    </SplitLayout>
+  );
 }
